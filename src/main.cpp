1b7f624c4955211bdb3ca4db608a929bf72843a6
Jose Maia <maianeto2014@gmail.com>
2023-05-17 21:30:29 -0300

    Implemented time measurement

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <algorithm>
 #include <iterator>
+#include <chrono>
+#include <ctime>
 
 #include "sorting.h"
 
@@ -8,20 +10,34 @@ using namespace std;
 
 // Driver program to test sorting functions
 int main(){
-	int arr[] = {64, 25, 12, 22, 11, 37, 543, 345, 190, 142, 191, 54, 123, 43, 234145};
-	int n = sizeof(arr)/sizeof(arr[0]);
+	int sz = 16000;    // Generate an array with 16000 elements
+    int arr[sz];
+    srand(time(NULL));
 
-	int* vec = arr;
+    // Fill the array
+    for(int i = 0; i < sz; i++)
+        arr[i] = rand()%900;  // Generate number between 0 to 900
 
-	cout << get_max(vec, vec + n) << endl;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int* vec = arr;
 
-	cout << "Unsorted array: \n";
-	printArray(vec, vec + n);
+    // cout << "Unsorted array: \n";
+    // printArray(vec, vec + n);
 
-	selection(vec, vec + n);
+    std::chrono::time_point<std::chrono::system_clock> start, end;
 
-	cout << "Sorted array: \n";
-	printArray(vec, vec + n);
-    
+    start = std::chrono::system_clock::now();
+    selection(vec, vec + n);
+    end = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+    // cout << "Sorted array: \n";
+    // printArray(vec, vec + n); 
+
+    std::cout << "finished computation at " << std::ctime(&end_time)
+            << "elapsed time: " << elapsed_seconds.count() << "s\n";
+
 	return 0;
 }
